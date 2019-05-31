e86157151dd72bd6bbe063139c17e986c790e48d
nicol <nicol@NE.fritz.box>
2019-05-31 14:49:16 +0200

    erstmal nur dropdown mit auswahl ob mitarbeiter oder kunde (mit zuweisung zum richtigen menue)

--- EShop140319/src/shop/local/ui/gui/screens/LoginScreen.java
+++ EShop140319/src/shop/local/ui/gui/screens/LoginScreen.java
@@ -1,41 +1,119 @@
 package shop.local.ui.gui.screens;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
 import shop.local.ui.gui.ShopClientGUI;
 import shop.local.valueobjects.Kunde;
 import shop.local.valueobjects.Mitarbeiter;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
 
 public class LoginScreen extends Screen {
 
+	JFrame frame;
 	JButton button;
+	JLabel labelId;
+	JLabel labelPw;
+	JLabel reminder;
+	JComboBox cmbAuswahlListe;
+	JTextField id;
+	JTextField passwort;
+	
 	
 	public LoginScreen(ShopClientGUI gui) {
 		super(gui);
 	}
-
+	
 	@Override
 	protected void InitializePanel() {
+		setLayout(new BorderLayout());
+		//Dropdown
+		String[] auswahl = {"Auswahl","Kunde","Mitarbeiter"};
+		cmbAuswahlListe = new JComboBox(auswahl);
+
+		
+		//Label
+		labelId = new JLabel("Id: ");
+		//labelId.setBounds(1,10,100,45);
+	
+		labelPw = new JLabel("Passwort: ");
+		//labelPw.setBounds(1, 10, 100, 45);
+		
+		reminder = new JLabel();
+		//Text
+		id = new JTextField();
+		//id.setBounds(214,98,154,23);
+		//frame.getContentPane().add(id);
+		//id.setColumns(10);
+		
+		
+		
+		passwort = new JTextField ();
 		
 		button = new JButton("Ok");
+		//button.setBounds(1, 10, 100, 80);
 		
-		button.addActionListener(new ActionListener() {
+
+		//add(labelId);
+		//add(labelPw);
+		add(reminder);
+
+		cmbAuswahlListe.setSize(cmbAuswahlListe.getPreferredSize());
+		add(cmbAuswahlListe, BorderLayout.NORTH);
+		
+		cmbAuswahlListe.setSelectedIndex(0);
+		cmbAuswahlListe.addActionListener(new ActionListener() {
+ 
 			
-			@Override
+			@Override	
 			public void actionPerformed(ActionEvent e) {
-				Mitarbeiter mitarbeiter = Shop().loginUeberpruefungMitarbeiter(0,"123");
-//				Kunde kundeImShop = Shop().loginUeberpruefungKunde(0, "123");
-//				gui.getKunde().SetKunde(kundeImShop);
-				gui.getMitarbeiter().SetMitarbeiter(mitarbeiter);
+				if (e.getSource() == cmbAuswahlListe) {
+					JComboBox cb = (JComboBox)e.getSource();
+					String msg = (String)cb.getSelectedItem();
+					
+					switch(msg) {
+						case "Auswahl": 
+							reminder.setText("Geben Sie an ob Sie ein Mitarbeiter oder Kunde sind.");
+							break;
+						case "Kunde":
+							button.addActionListener(new ActionListener() {
+								@Override
+								public void actionPerformed(ActionEvent e) {
+									Kunde kundeImShop = Shop().loginUeberpruefungKunde(0, "123");
+									gui.getKunde().SetKunde(kundeImShop);
+									gui.ChangeScreen(ScreenState.Kunde);
+								}
+							});
+							break;
+						case "Mitarbeiter":
+							button.addActionListener(new ActionListener() {
+								@Override
+								public void actionPerformed(ActionEvent e) {
+									Mitarbeiter mitarbeiterImShop = Shop().loginUeberpruefungMitarbeiter(0, "123");
+									gui.getMitarbeiter().SetMitarbeiter(mitarbeiterImShop);
+									gui.ChangeScreen(ScreenState.Mitarbeiter);
+								}
+							});
+							break;
+						default: System.out.println("Ein Fehler ist aufgetreten.");
+					}
+					
+				}
 				
-				gui.ChangeScreen(ScreenState.Mitarbeiter);
 			}
 		});
+		
+		
 		add(button, BorderLayout.SOUTH);
+
+		
 	}
 
 	
